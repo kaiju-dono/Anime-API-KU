e97df7e5e469cf973442f6e97a383b702cc2cb90
Stephen Paul Svetlovics <battousai012083@gmail.com>
2022-02-16 21:38:19 -0500

    all links fixed and formatting updated

--- animeScript.js
+++ animeScript.js
@@ -15,35 +15,44 @@ fetch("https://jikan1.p.rapidapi.com/genre/anime/1/1", {
     const anime = data.anime;
     console.log(anime[27].img_url);
     let tempAnimeHTML = `
-        <div class="col-4 border border-5px">
+        <div style="margin:10px;height:500px;width:365px;background-image: url('./char_images/bleach.jpg');background-size:cover" class="col-4 border border-5px">
         <h1 class="text-danger">${anime[27].title}</h1><br>
-        <img src="https://cdn.myanimelist.net/images/anime/3/40451.jpg" style="width:200px"> <br>
-        <textarea style="width:400px;height:200px" class="lead text-success">${anime[27].synopsis}</textarea><br>
-        <a href="">Characters</a>
+        <div style="position:absolute;top:380px">
+        <textarea style="width:340px;height:200px" class="lead text-success">${anime[27].synopsis}</textarea><br>
+        <a style="text-shadow: 2px 2px 2px black;" class="text-warning" href="./bleach.html">Characters</a>
         </div>
-        <div class="col-4 border border-5px">
+        </div>
+
+        <div style="margin:10px;height:500px;width:365px;background-image: url('./char_images/sao.jpg');background-size:cover" class="col-4 border border-5px">
         <h1 class="text-danger">${anime[3].title}</h1><br>
-        <img src="https://cdn.myanimelist.net/images/anime/11/39717.jpg"> <br>
-        <textarea style="width:400px;height:200px" class="lead text-success">${anime[3].synopsis}</textarea><br>
-        <a href="">Characters</a>
+        <div style="position:absolute;top:380px">
+        <textarea style="width:340px;height:200px" class="lead text-success">${anime[3].synopsis}</textarea><br>
+        <a style="text-shadow: 2px 2px 2px black;" class="text-warning" href="./SAO.html">Characters</a>
         </div>
-        <div class="col-4 border border-5px">
+        </div>
+
+        <div style="margin:10px;height:500px;width:365px;background-image: url('./char_images/sao.jpg');background-size:cover" class="col-4 border border-5px">
         <h1 class="text-danger">${anime[8].title}</h1><br>
-        <img src="https://cdn.myanimelist.net/images/anime/1286/99889.jpg"> <br>
-        <textarea style="width:400px;height:200px" class="lead text-success">${anime[8].synopsis}</textarea><br>
-        <a href="">Characters</a>
+        <div style="position:absolute;top:380px">
+        <textarea style="width:340px;height:200px" class="lead text-success">${anime[8].synopsis}</textarea><br>
+        <a style="text-shadow: 2px 2px 2px black;" class="text-warning" href="./kimetsuNoYaiba.html">Characters</a>
         </div>
-        <div class="col-4 border border-5px">
+        </div>
+
+        <div style="margin:10px;height:500px;width:365px;background-image: url('./char_images/cowboyBebop.jpg');background-size:cover" class="col-4 border border-5px">
         <h1 class="text-danger">${anime[29].title}</h1><br>
-        <img src="https://cdn.myanimelist.net/images/anime/4/19644.jpg"> <br>
-        <textarea style="width:400px;height:200px" class="lead text-success">${anime[29].synopsis}</textarea><br>
-        <a href="">Characters</a>
+        <div style="position:absolute;top:910px">
+        <textarea style="width:340px;height:200px" class="lead text-success">${anime[29].synopsis}</textarea><br>
+        <a style="text-shadow: 2px 2px 2px black;" class="text-warning" href="cowboyBebop.html">Characters</a>
         </div>
-        <div class="col-4 border border-5px">
+        </div>
+
+        <div style="margin:10px;height:500px;width:365px;background-image: url('./char_images/dbz.jpg');background-size:cover" class="col-4 border border-5px">
         <h1 class="text-danger">${anime[61].title}</h1><br>
-        <img src="https://cdn.myanimelist.net/images/anime/1607/117271.jpg"> <br>
-        <textarea style="width:400px;height:200px" class="lead text-success">${anime[61].synopsis}</textarea><br>
-        <a href="">Characters</a>
+        <div style="position:absolute;top:910px">
+        <textarea style="width:340px;height:200px" class="lead text-success">${anime[61].synopsis}</textarea><br>
+        <a style="text-shadow: 2px 2px 2px black;" class="text-warning" href="cowboyBebop.html">Characters</a>
+        </div>
         </div>
     `
 
